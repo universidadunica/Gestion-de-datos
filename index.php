38f4e7320d4304837536eb22ecd7e397a1c0dd52
Programadores Avazados <38713870+universidadunica@users.noreply.github.com>
2019-01-24 01:51:06 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <!-- Estos links son los que le daran estilos al proyecto, bien aqui puse bootstrap pero tambien podemos hacer los enlaces
+        a los estilos que querramos -->
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
+    <script src="bootstrap/js/jquery.min.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+
+  </head>
+  <body>
+    <h1 class="text-center text-primary">Bienvenidos !</h1>
+    
+  </body>
+</html>
